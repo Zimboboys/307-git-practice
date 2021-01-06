39a326487d936faf8a8d226bb50427a0760f238d
Ethan Zimbelman <itszimbo@gmail.com>
2021-01-06 10:49:30 -0800

    added years at Cal Poly

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello world! I've been at Cal Poly for 3 years");
   }
 }
